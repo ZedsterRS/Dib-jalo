4766217110adb4faecbc4b21b34afba395b743d0
Zedsters <108711210+ZedsterRS@users.noreply.github.com>
2022-07-07 00:49:18 -0400

    Add files via upload

--- events.js
+++ events.js
@@ -1,2 +1,35 @@
-var canvitas = document.getElementById("canvas_dibujo");
-var papel = canvitas.getContext("2d");
+var canvas = document.getElementById("canvas_dibujo");
+var papel = canvas.getContext("2d");
+var button = document.getElementById("button_azul");
+canvas.addEventListener("mousedown", punto_inicial);
+canvas.addEventListener("mousemove", movimiento_cursor);
+canvas.addEventListener("mouseup", punto_final);
+var estado;
+var x;
+var y;
+
+function punto_inicial(evento)
+{
+    console.log("punto inicial");
+}
+
+function movimiento_cursor(evento)
+{
+    console.log("el muose se ta moveindo crack");
+}
+
+function punto_final(evento)
+{
+    console.log("puntoFinal");
+}
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+    papel.beginPath();
+    papel.lineWidth = 2;
+    papel.strokeStyle = color;
+    papel.moveTo(xinicial, yinicial);
+    papel.lineTo(xfinal, yfinal);
+    papel.stroke();
+    papel.closePath;
+}
